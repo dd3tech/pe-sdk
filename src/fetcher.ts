779451478c519f64adde8d9cc4d56e8c884bbac6
tbor00 <timoteo.borgobello@dd3tech.mx>
2024-05-28 20:25:15 -0300

    feat: add comments to clientoptions and add optional chaining on headers

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,15 +1,46 @@
 import fetch from 'isomorphic-unfetch'
+import ApiError from './error'
 import { PriceEngineVersion } from './types'
 
+const {
+  PRICE_ENGINE_API_KEY = '',
+  PRICE_ENGINE_BASE_URL = 'https://api.dd360.mx'
+} = process?.env || {}
+
 export interface ClientOptions {
-  apiKey: string
+  /**
+   * Defaults to process.env['PRICE_ENGINE_API_KEY'].
+   */
+  apiKey?: string | undefined
+  /**
+   * The API version to use.
+   *
+   * @default 'v9'
+   */
   version?: PriceEngineVersion | undefined
+  /**
+   * Override the default base URL for the API, e.g., "https://api.dd360.mx"
+   *
+   * Defaults to process.env['PRICE_ENGINE_BASE_URL'].
+   */
   baseURL?: string
+  /**
+   * The maximum amount of time (in milliseconds) that the client should wait for a response
+   * from the server before timing out a single request.
+   *
+   * Note that request timeouts are retried by default, so in a worst-case scenario you may wait
+   * much longer than this timeout before the promise succeeds or fails.
+   */
   timeout?: number
+  /**
+   * The maximum number of times that the client will retry a request in case of a
+   * temporary failure, like a network error or a 5XX error from the server.
+   * @default 2
+   */
   maxRetries?: number
 }
 
-export class Base {
+export class BaseFetcher {
   public readonly version: PriceEngineVersion = 'v9'
   private readonly apiKey: string
   private readonly baseURL: string
@@ -17,11 +48,6 @@ export class Base {
   private readonly maxRetries: number
 
   constructor(clientOptions: ClientOptions) {
-    const {
-      PRICE_ENGINE_API_KEY = '',
-      PRICE_ENGINE_BASE_URL = 'https://api.dd360.mx'
-    } = process?.env || {}
-
     this.baseURL = clientOptions.baseURL || PRICE_ENGINE_BASE_URL
     this.apiKey = clientOptions.apiKey || PRICE_ENGINE_API_KEY
     this.version = clientOptions.version || this.version
@@ -55,8 +81,9 @@ export class Base {
       ...options,
       signal: controller.signal
     })
+
     clearTimeout(id)
-    return response.json() as T
+    return response?.json() as T
   }
 
   protected async request<T>(
@@ -65,7 +92,7 @@ export class Base {
   ): Promise<T> {
     const url: string = this.getURL(endpoint)
     const headers = {
-      ...options.headers,
+      ...options?.headers,
       Authorization: this.getAuthorizationHeader(),
       'Content-Type': 'application/json'
     }
@@ -77,16 +104,18 @@ export class Base {
     for (let attempt = 0; attempt <= this.maxRetries; attempt++) {
       try {
         const response = await this.requestWithTimeout<Response>(url, config)
-        if (response.ok) {
+        if (response?.ok) {
           return response.json() as T
         }
-        throw new Error(response.statusText)
+
+        throw new ApiError(response?.statusText)
       } catch (error) {
         if (attempt === this.maxRetries) {
           throw error
         }
       }
     }
+
     throw new ApiError('Request failed after max retries')
   }
 }
